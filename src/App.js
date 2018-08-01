121ab6a837cae344fc42e9a70dded0e7e8e30fc3
Nadia Essoubai <nadia.ess.94@hotmail.fr>
2018-08-01 17:38:49 +0200

    initial commit

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+import { GoogleApiWrapper } from 'google-maps-react';
+import { locations } from './data/locations';
+import { mapStyle } from './data/mapStyle';
+import './index.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+      </div>
+    );
+  }
+}
+
+export default App;
---
+++ src/data/locations.js
@@ -0,0 +1,43 @@
+export const locations = [{
+                    title: "Jardin d'agronomie tropicale",
+                    location : {lat: 48.83315839999999, lng: 2.467029700000012},
+                    address: "45 Avenue de la Belle Gabrielle, 75012 Paris",
+                    img: './images/jardin-agronomie-tropicale.jpg'
+                },
+                {
+                    title: "Lac de Créteil",
+                    location : {lat: 48.7766056, lng: 2.448140100000046},
+                    address: "9 Rue Jean Gabin, 94000 Créteil",
+                    img: './images/lac-de-creteil.jpg'
+                },
+                {
+                    title: "Parc départemental de la Roseraie",
+                    location : {lat: 48.7763046, lng: 2.334603300000026},
+                    address: "Rue Robert Watel 94240 L'Haÿ-les-Roses",
+                    img: './images/parc-roseraie.jpg'
+                },
+                {
+                    title: "Parc des Cormailles",
+                    location : {lat: 48.8153173, lng: 2.3884213000000045},
+                    address: "Avenue Georges Gosnat, 94200 Ivry-sur-Seine",
+                    img: './images/parc-des-cormailles.jpg'
+                },
+                {
+                    title: "Parc zoologique de Paris",
+                    location : {lat: 48.833364, lng: 2.414208099999996},
+                    address: "Avenue Daumesnil, 75012 Paris",
+                    img: './images/zoo-vincennes.jpg'
+                },
+                {
+                    title: "Parc de Choisy-le-Roi",
+                    location : {lat: 48.7592392, lng: 2.423343700000032},
+                    address: "125 Avenue de Villeneuve St-Georges, 94600 Choisy-le-Roi",
+                    img: './images/parc-choisy.jpg'
+                },
+                {
+                    title: "Parc de Sceaux",
+                    location : {lat: 48.7741979, lng: 2.295680100000027},
+                    address: "8 Avenue Claude Perrault, 92330 Sceaux",
+                    img: './images/parc-sceaux.jpg'
+                }
+            ]
